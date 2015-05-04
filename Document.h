89181363dc773372d21d4b92e0bc5180b0cd2390
Filip Zelic <zelic.filip@gmail.com>
2015-05-04 10:54:44 +0200

    Open existing file and saving file added

--- Document.h
+++ Document.h
@@ -3,7 +3,15 @@
 #include <AppKit/AppKit.h>
 #include <AppKit/NSDocument.h>
 
+@class NSDocument; 
+NSString * GSTextDocumentType = @"GSTextDocumentType";
 @interface Document : NSDocument
 {
+	@protected
+    id textView;
+    NSString* fileContents;
 }
+- (BOOL) loadDataRepresentation:(NSData*)representation ofType:(NSString*)type;
+- (NSData*) dataRepresentationOfType: (NSString*)type; 
+- (NSString*) windowNibName;
 @end
--- Document.m
+++ Document.m
@@ -4,6 +4,34 @@
 #include "Document.h"
 
 @implementation Document
+- (BOOL) loadDataRepresentation:(NSData*) representation ofType:(NSString*)type
+{
+   if (![type isEqual:GSTextDocumentType])
+       return NO;
+
+   if (self->fileContents) RELEASE(self->fileContents); 
+
+   self->fileContents = [NSString alloc];
+   self->fileContents = [self->fileContents initWithBytes: [representation bytes]
+                                                   length: [representation length]
+                                                 encoding: NSASCIIStringEncoding]; 
+
+   if (!self->fileContents)
+       return NO; 
+
+   [self->textView setString: fileContents];
+   return YES;
+}
+
+- (NSData*) dataRepresentationOfType:(NSString*)type
+{
+    if (![type isEqual:GSTextDocumentType])
+        return nil; 
+
+    NSData * stringData =  [[textView string] dataUsingEncoding:NSASCIIStringEncoding];  
+    return stringData; 
+}
+
 - (NSString *) windowNibName
 {
    return @"Document.gorm";
